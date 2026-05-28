e7cf47f6011fa88b653495abddbe0e6f5bd2c342
Alice_Xu <xgb.xu@qq.com>
2026-05-29 00:32:49 +0800

    feat: 解决构建问题

--- packages/business-components/.fatherrc.js
+++ packages/business-components/.fatherrc.js
@@ -2,9 +2,9 @@ export default {
   umd: {
     entry: 'src/index', // 默认构建入口文件
     output: 'dist',
-    externals: [
+    externals: {
       react: 'React',
       'react-dom': 'ReactDOM',
-    ]
+    }
   }
 }
